a36b1d5e3fc9cfe454fc0df033c28bdb4c3f2adc
Ilja-Mi <wovanmilja@gmail.com>
2026-03-18 22:15:09 +0200

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,58 @@
+const API = "https://tinkr.tech/sdb/my_database";
+
+async function loadTasks(){
+
+    const res = await fetch(API);
+    const data = await res.json();
+
+    const list = document.getElementById("taskList");
+    list.innerHTML = "";
+
+    data.forEach(task => {
+
+        const li = document.createElement("li");
+        li.textContent = task.text;
+
+        const del = document.createElement("button");
+        del.textContent = "X";
+
+        del.onclick = () => deleteTask(task.id);
+
+        li.appendChild(del);
+        list.appendChild(li);
+
+    });
+
+}
+
+async function addTask(){
+
+    const input = document.getElementById("taskInput");
+    const text = input.value;
+
+    if(!text) return;
+
+    await fetch(API,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({text:text})
+    });
+
+    input.value="";
+    loadTasks();
+
+}
+
+async function deleteTask(id){
+
+    await fetch(API + "/" + id,{
+        method:"DELETE"
+    });
+
+    loadTasks();
+
+}
+
+loadTasks();
